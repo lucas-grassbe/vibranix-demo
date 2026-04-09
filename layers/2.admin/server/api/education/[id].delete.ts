45c38817958d34052ce0b0a407c97159f8f8be04
Tiago Brilhante <tiagobrilhantemania@gmail.com>
2026-04-09 08:36:04 -0400

    chore: add example comment in `env.example` at brilhante Branch

--- layers/2.admin/server/api/education/[id].delete.ts
+++ layers/2.admin/server/api/education/[id].delete.ts
@@ -2,7 +2,7 @@ import { deleteEducation } from '../../service/educationService'
 
 export default defineEventHandler(async (event) => {
 const id = Number(getRouterParam(event, 'id'))
-
+  
   if (!id) {
     throw createError({ statusCode: 400, message: 'Invalid ID' })
   }
